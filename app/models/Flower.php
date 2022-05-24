972d8372f6156dd2e8838a7828199f5d14ecf58d
kondo1173 <migumeudonko@icloud.com>
2022-05-24 14:56:34 +0900

    Initial commit

---
+++ app/models/Flower.php
@@ -0,0 +1,16 @@
+<?php
+
+use Phalcon\Mvc\Model;
+
+class Flower extends Model
+{
+    public $id;
+    public $name;
+    public $color;
+    public $category;
+    public $info;
+    public $path;
+    public $stock;
+    public $price;
+
+}
